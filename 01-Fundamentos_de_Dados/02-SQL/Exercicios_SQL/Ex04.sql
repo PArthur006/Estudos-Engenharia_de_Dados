b7800f18d24e4dfd094d1ee796222bd96d7f7c2d
PArthur006 <parthur.rodrigues06@gmail.com>
2025-11-03 14:19:39 -0300

    HashTag Treinamentos
    Exercícios Resolvidos

--- 01-Fundamentos_de_Dados/02-SQL/Exercicios_SQL/Ex04.sql
+++ 01-Fundamentos_de_Dados/02-SQL/Exercicios_SQL/Ex04.sql
@@ -1,4 +1,8 @@
 -- a) Descubra o total de custo da tabela de pedidos.
+SELECT SUM(Custo_Venda) AS "Custo Total" FROM pedidos;
 -- b) Descubra a quantidade total de lojas.
+SELECT COUNT(Loja) FROM lojas;
 -- c) Descubra o custo médio dos produtos (de acordo com a tabela de produtos).
+SELECT AVG(Custo_Unit) AS "Custo Médio dos Produtos" FROM produtos;
 -- d) Descubra a quantidade mínima e máxima de funcionários das lojas (faça isso em um único SELECT).
+SELECT MIN(`Num_Funcionarios`) AS "Qtd. Mínima de Funcionários", MAX(`Num_Funcionarios`) AS "Qtd. Máxima de Funcionários" FROM lojas;
