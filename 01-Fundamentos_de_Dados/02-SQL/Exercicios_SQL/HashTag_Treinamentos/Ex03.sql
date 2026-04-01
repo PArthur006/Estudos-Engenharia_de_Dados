04a08a2113eefdb4a06014c2a35bbb56703a33ea
PArthur006 <parthur.rodrigues06@gmail.com>
2026-04-01 14:01:57 -0300

    Remove arquivos antigos e adciona gitignore

--- 01-Fundamentos_de_Dados/02-SQL/Exercicios_SQL/HashTag_Treinamentos/Ex03.sql
+++
@@ -1,8 +0,0 @@
--- a) Faça um filtro na tabela de produtos e retorne apenas os produtos com custo maior ou igual a 200.
-SELECT * FROM produtos
-WHERE Custo_Unit >= 200
-ORDER BY Custo_Unit;
-
--- b) Faça um filtro na tabela de produtos e retorne apenas os produtos da marca "DELL"
-SELECT * FROM produtos
-WHERE Marca_Produto = 'DELL';
